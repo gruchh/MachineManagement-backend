40907c64bf76915a17e8e0f67bc73b82d1449b68
Kuba <kubol2@gmail.com>
2023-08-14 10:01:05 +0200

    Create Interfaces and Services to save entity #3

--- src/main/java/com/gruchh/maintenance/Service/WorkerServiceImpl.java
+++ src/main/java/com/gruchh/maintenance/Service/WorkerServiceImpl.java
@@ -1,6 +1,5 @@
 package com.gruchh.maintenance.Service;
 
-import com.gruchh.maintenance.Entity.WorkOrder;
 import com.gruchh.maintenance.Entity.Worker;
 import com.gruchh.maintenance.Repository.WorkerRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +9,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Service
-public class WorkerServiceImpl implements WorkerService{
+public class WorkerServiceImpl implements WorkerService {
 
     private final WorkerRepository workerRepository;
 
